6f4bdc3f27092083aa27918d84513c724cb1cf74
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-15 15:05:33 +0900

    docs: Update README.md

--- src/common/constants.js
+++ src/common/constants.js
@@ -50,7 +50,7 @@ const BADGES = Object.freeze([
 ]);
 
 const INPUT = Object.freeze({
-  event_date: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
+  event_date: '안녕하세요! 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
   menu_count: '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
 });
 
@@ -63,7 +63,7 @@ const DISCOUNT = Object.freeze({
 });
 
 const OUTPUT = Object.freeze({
-  preview: `12월 $date일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
+  preview: `12월 $date일에 식당에서 받을 이벤트 혜택 미리 보기!`,
   order_menu: `\n<주문 메뉴>\n$menuList`,
   total_order_before_discount: `\n<할인 전 총주문 금액>\n$totalAmount원`,
   bonus_Menu: `\n<증정 메뉴>\n$bonusMenu`, 
